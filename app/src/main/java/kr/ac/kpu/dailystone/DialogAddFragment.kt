8bc8632b10aedc36fedeb1f9746e8422b5281d35
Minu <rlaalsdn160@naver.com>
2020-07-27 18:28:52 +0900

    -.
    .

--- app/src/main/java/kr/ac/kpu/dailystone/DialogAddFragment.kt
+++ app/src/main/java/kr/ac/kpu/dailystone/DialogAddFragment.kt
@@ -24,6 +24,11 @@ class DialogAddFragment(context: Context) : Dialog(context) {
     private var mAuth: FirebaseAuth? = null
     val database : FirebaseDatabase = FirebaseDatabase.getInstance()
     private lateinit var db: DatabaseReference
+    private lateinit var level : Any
+    private lateinit var diary : Any
+    private val current: LocalDate = LocalDate.now()
+    val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+    val formatted = current.format(formatter)
     var user: FirebaseUser?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,13 +42,49 @@ class DialogAddFragment(context: Context) : Dialog(context) {
         var uid = user?.uid.toString()
         readID(uid)
 
+
+        dhBtnH1.setOnClickListener{
+            dhEdHl.setText("5")
+            level = dhEdHl.text.toString()
+        }
+
+        dhBtnH2.setOnClickListener{
+            dhEdHl.setText("10")
+            level = dhEdHl.text.toString()
+        }
+
+        dhBtnH3.setOnClickListener{
+            dhEdHl.setText("15")
+            level = dhEdHl.text.toString()
+        }
+
         dhBtnYes.setOnClickListener {
-            Toast.makeText(context,"확인 테스트",Toast.LENGTH_SHORT)
+            // 데이터베이스에 저장
+            onWriteDBPost()
+            Toast.makeText(context,"저장 완료",Toast.LENGTH_SHORT)
+            dismiss()
         }
         dhBtnNo.setOnClickListener {
-            Toast.makeText(context,"취소 테스트",Toast.LENGTH_SHORT)
+            Toast.makeText(context,"취소",Toast.LENGTH_SHORT)
             dismiss();
         }
+    }
+
+    fun onWriteDBPost() {
+        db = Firebase.database.reference
+        var user = FirebaseAuth.getInstance().currentUser
+        level = dhEdHl.text.toString()
+        diary = dhEdDiary.text.toString()
+        //val myRef = database.getReference("posts")
+        val myRef = database.getReference(user?.uid.toString())
+
+        val postValues: HashMap<String, Any> = HashMap()
+        postValues["date"] = formatted
+        postValues["level"] = level
+        postValues["diary"] = diary
+        /* id = readID()
+         postValues["id"] = ++id*/
+        myRef.setValue(postValues)
     }
 
     fun readID(uid:String): String {

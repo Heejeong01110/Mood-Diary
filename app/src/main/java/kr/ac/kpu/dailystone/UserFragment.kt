7d73fc3b35c517fa62269fba49e426f368980161
Minu <rlaalsdn160@naver.com>
2020-07-27 15:54:14 +0900

    로그아웃
    auth 및 토스트 메시지 추가

--- app/src/main/java/kr/ac/kpu/dailystone/UserFragment.kt
+++ app/src/main/java/kr/ac/kpu/dailystone/UserFragment.kt
@@ -5,10 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_userinfo.*
+import java.util.Calendar.getInstance
 
 
 class UserFragment : Fragment() {
+    //private var auth: FirebaseAuth? = null
+    private lateinit var auth : FirebaseAuth
      companion object { // 상수 역할
 
         fun newInstance(): UserFragment {
@@ -19,7 +26,11 @@ class UserFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         userBtnLogout.setOnClickListener {
+          auth = Firebase.auth
+            auth.signOut()
             activity?.finish()
+            Toast.makeText(activity, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show()
+
         }
     }
     override fun onCreateView(

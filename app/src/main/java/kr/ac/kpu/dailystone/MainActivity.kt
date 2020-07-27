9efb7114376abd7eb4b3514a97627ddb1df68239
Heejeong01110 <123rkdrkd@naver.com>
2020-07-27 19:14:22 +0900

    에러수정1

--- app/src/main/java/kr/ac/kpu/dailystone/MainActivity.kt
+++ app/src/main/java/kr/ac/kpu/dailystone/MainActivity.kt
@@ -4,27 +4,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentStatePagerAdapter
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.activity_main.viewPager
 import kotlinx.android.synthetic.main.fragment_main.*
-import kotlinx.android.synthetic.main.fragment_userinfo.*
 
 class MainActivity : AppCompatActivity() {
 
     val adapter = MainAdapter(supportFragmentManager)
-    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewPager.adapter = adapter
         dots_indicator.setViewPager(viewPager)
-
         /*adapter.addFragment(MainFragment())
         adapter.addFragment(MonthFragment())
         adapter.addFragment(UserFragment())*/
--- app/src/main/java/kr/ac/kpu/dailystone/UserFragment.kt
+++ app/src/main/java/kr/ac/kpu/dailystone/UserFragment.kt
@@ -1,5 +1,6 @@
 package kr.ac.kpu.dailystone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,42 +8,57 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_userinfo.*
-import java.util.Calendar.getInstance
 
 
 class UserFragment : Fragment() {
-    //private var auth: FirebaseAuth? = null
-    private lateinit var auth : FirebaseAuth
-     companion object { // 상수 역할
+    private lateinit var mAuth : FirebaseAuth
+    companion object { // 상수 역할
 
         fun newInstance(): UserFragment {
-             return UserFragment()
-         }
-     }
+            return UserFragment()
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        userBtnLogout.setOnClickListener {
-          auth = Firebase.auth
-            auth.signOut()
-            activity?.finish()
-            Toast.makeText(activity, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show()
+        mAuth = FirebaseAuth.getInstance()
+
+        userBtninfo.setOnClickListener {
+            val intent = Intent(context, UserInfoActivity::class.java)
+            startActivity(intent)
+        }
+
+        userBtnUpdate.setOnClickListener {
 
         }
+
+        userBtnReset.setOnClickListener {
+
+        }
+
+        userBtnLogout.setOnClickListener {      //클릭 시 로그아웃
+            signOut()
+        }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_userinfo,container,false)
+        return inflater.inflate(R.layout.fragment_userinfo, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+    }
+
+    private fun signOut(){
+        val intent = Intent(context, LoginActivity::class.java)
+        FirebaseAuth.getInstance().signOut()
+        startActivity(intent)
+        activity?.finish()
     }
 
 }

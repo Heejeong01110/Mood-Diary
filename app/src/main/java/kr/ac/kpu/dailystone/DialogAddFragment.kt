060042953701df3d0c09c00ca8569c8af31f85da
Minu <rlaalsdn160@naver.com>
2020-07-28 15:59:49 +0900

    .
    .

--- app/src/main/java/kr/ac/kpu/dailystone/DialogAddFragment.kt
+++ app/src/main/java/kr/ac/kpu/dailystone/DialogAddFragment.kt
@@ -4,12 +4,8 @@ import android.app.Dialog
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.fragment.app.DialogFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
@@ -17,8 +13,9 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.dialog_happy.*
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.HashMap
 
 @RequiresApi(Build.VERSION_CODES.O)
 class DialogAddFragment(context: Context) : Dialog(context) {
@@ -29,8 +26,12 @@ class DialogAddFragment(context: Context) : Dialog(context) {
     private lateinit var diary : Any
 
     private val current: LocalDate = LocalDate.now()
-    val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
-    val formatted = current.format(formatter)
+    val formatterYear = DateTimeFormatter.ofPattern("yyyy")
+    val yearformatted = current.format(formatterYear)
+    val formatterMonth = DateTimeFormatter.ofPattern("mm")
+    val monthformatted = current.format(formatterMonth)
+    val formatterDay = DateTimeFormatter.ofPattern("dd")
+    val dayformatted = current.format(formatterDay)
     var user: FirebaseUser?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,15 +61,22 @@ class DialogAddFragment(context: Context) : Dialog(context) {
             level = dhEdHl.text.toString()
         }
 
+        dhBtnDice.setOnClickListener {
+            var rnd = Random()
+            var num = rnd.nextInt(15)
+            dhEdHl.setText(num.toString())
+            level = dhEdHl.text.toString()
+
+        }
+
         dhBtnYes.setOnClickListener {
             // 데이터베이스에 저장
             onWriteDBPost()
-            Toast.makeText(context,"저장 완료",Toast.LENGTH_SHORT)
             dismiss()
         }
         dhBtnNo.setOnClickListener {
-            Toast.makeText(context,"취소",Toast.LENGTH_SHORT)
             dismiss();
+            Toast.makeText(context,"취소",Toast.LENGTH_SHORT)
         }
     }
 
@@ -87,12 +95,13 @@ class DialogAddFragment(context: Context) : Dialog(context) {
         /* id = readID()
          postValues["id"] = ++id*/
         myRef.setValue(postValues)
+        Toast.makeText(context,"저장 완료",Toast.LENGTH_SHORT)
     }
 
     fun readID(uid:String): String {
         db = Firebase.database.reference
         var Did: String="1"
-        db.child(uid).child("20200727").runTransaction(object : Transaction.Handler {
+        db.child(uid).child(formatterYear.toString()).child(formatterMonth.toString()).runTransaction(object : Transaction.Handler {
             override fun doTransaction(mutableData: MutableData): Transaction.Result {
                 if((mutableData.child("Did").value as? Long)!=null){
                     Did= mutableData.child("Did").value as String

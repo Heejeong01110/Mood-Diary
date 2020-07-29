cd34c631b568dcd76c48805bffbf6d4f09d9e019
Contea <contea95@naver.com>
2020-07-29 23:25:26 +0900

    add month accumulation
    for루프를 통한 gridlayout에 동적으로 추가하는 함수 구현, 더 다듬어야한다.

--- app/src/main/java/kr/ac/kpu/dailystone/MonthFragment.kt
+++ app/src/main/java/kr/ac/kpu/dailystone/MonthFragment.kt
@@ -1,10 +1,22 @@
 package kr.ac.kpu.dailystone
 
+import android.app.ActionBar
+import android.app.Activity
+import android.content.res.Resources
+import android.hardware.display.DisplayManager
+import android.media.Image
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.Display
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_month.*
 
 
 class MonthFragment : Fragment() {
@@ -24,11 +36,28 @@ class MonthFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_month,container,false)
+        val rootView = inflater.inflate(R.layout.fragment_month,container,false)
+        for(i in 1..30) {
+            val gv: GridLayout = rootView.findViewById(R.id.monthGrid)
+            var width = Resources.getSystem().displayMetrics.widthPixels
+            var devicewidth = (width - 128) / 5
+            val iv: ImageView = ImageView(context)
+            iv.setImageResource(R.drawable.happy_level1)
+            val params = LinearLayout.LayoutParams(devicewidth, devicewidth)
+            iv.layoutParams = params
+            iv.setOnClickListener {
+                Toast.makeText(context, "clicked $i", Toast.LENGTH_SHORT).show()
+            }
+            gv.addView(iv)
+
+        }
+
+        return rootView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
     }
+
 
 }
